58e83bf12e3be551b67e9ec2f2b9c2f2a92b8276
Tony Karanja <fenixpiconi@gmail.com>
2017-11-09 20:55:57 -0600

    added react-router, material-ui and axios

--- final-project/src/index.js
+++ final-project/src/index.js
@@ -3,6 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter as Router } from 'react-router-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    (
+        <Router>
+            <MuiThemeProvider>
+                <App />
+            </MuiThemeProvider>
+        </Router>
+    ),
+    document.getElementById('root')
+);
 registerServiceWorker();
