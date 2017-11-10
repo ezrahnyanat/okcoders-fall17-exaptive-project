f6c5fc4133d39c8da74254bb238163157d6ffff1
Ezrah Nyanat <ezrahnyanat@gmail.com>
2017-11-10 16:31:33 -0600

    Added Links and Routes

--- final-project/src/display.js
+++ final-project/src/display.js
@@ -5,7 +5,7 @@ import RaisedButton from 'material-ui/RaisedButton'
 import Introduction from './Introduction.js'
 import ExaptiveComponents from './ExaptiveComponents.js'
 import Xaps from './Xaps.js'
-import { Link } from 'react-router-dom'
+import { Route, Switch, Link } from 'react-router-dom'
 
 
 class Display extends Component {
@@ -33,10 +33,16 @@ class Display extends Component {
                     open={this.state.open}
                     onRequestChange={(open) => this.setState({open})}
                     >
-                    <MenuItem>Introduction</MenuItem>
+                    <MenuItem><Link to = "./Introduction">Introduction</Link></MenuItem>
                     <MenuItem><Link to = "./ExaptiveComponents">Components</Link></MenuItem>
                     <MenuItem><Link to = "./Xaps">Xaps</Link></MenuItem>
                 </Drawer>
+                
+                <Switch>
+                    <Route exact path="/Introduction/" component={Introduction} />
+                    <Route exact path="/ExaptiveComponents" component={ExaptiveComponents} />
+                    <Route exact path="/Xaps" component={Xaps} />
+                </Switch>
             </div>
         )
     }
